4ea7d5914a5000a1de352affdbcba3697a92ff23
Keegan Campbell <kfcampbell@Keegans-MBP.hitronhub.home>
2019-11-23 10:53:50 -0800

    Remove unnecessary public method identifier.

--- scraper.ts
+++ scraper.ts
@@ -11,9 +11,17 @@ export class Scraper {
             console.error(error);
         }
     }
-    public async getPage(url: string): Promise<any> {
+
+    private async getPage(url: string): Promise<any> {
         const response = await axios.default.get(url);
         console.log(response.data);
         return response.data;
+    }
+
+    private parsePage(pageOutput: any): any {
+        // todo(kfcampbell): do something useful with the output here
+        // inspect the page for useful tags
+        // put the cheerio code snippet here
+        // make some magic happen
     }
 }
