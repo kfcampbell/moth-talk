afb8fdcb1946b340cae24c5cd6504f3dedcecbad
kfcampbell <Keegan.Campbell@microsoft.com>
2019-11-28 12:50:33 -0800

    Scrape the good stuff.

--- scraper.ts
+++ scraper.ts
@@ -27,21 +27,41 @@ export class Scraper {
         const days = $('.day');
         const months = $('.month');
         const numericDays = $('.numeric-day');
-        const eventTimes = $('.')
+        const eventTimes = $('.event-time');
+
+        let formattedDays: string[] = [];
+        for(let i = 0; i < days.length; i++) {
+            formattedDays.push(days[i].children[0].data.trim());
+        }
+
+        let formattedMonths: string[] = [];
+        for(let i = 0; i < months.length; i++) {
+            formattedMonths.push(months[i].children[0].data.trim());
+        }
+
+        let formattedNumericDays: string[] = [];
+        for(let i = 0; i < numericDays.length; i++) {
+            formattedNumericDays.push(numericDays[i].children[0].data.trim());
+        }
+
+        let formattedEventTimes: string[] = []
+        for(let i = 0; i < eventTimes.length; i++) {
+            formattedEventTimes.push(eventTimes[i].children[0].data.trim());
+        }
 
         // event type, theme, venue, time
         const eventContents = $('.event-content');
         let eventTypes = [];
         let eventThemes = [];
         for(let i = 0; i < eventContents.length; i++) {
-            eventTypes.push(eventContents[i].children[1].children[0].data);
-            eventThemes.push(eventContents[i].children[3].children[0].children[0].data);
+            eventTypes.push(eventContents[i].children[1].children[0].data.trim());
+            eventThemes.push(eventContents[i].children[3].children[0].children[0].data.trim());
         }
 
         const venueDetails = $('.venue-detail');
         let venues = [];
         for(let i = 0; i < venueDetails.length; i++) {
-            venues.push(venueDetails[i].children[1].children[0].data);
+            venues.push(venueDetails[i].children[1].children[0].data.trim());
         }
     }
 }
